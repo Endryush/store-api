4be6ab897e1cb77307e2a62c1f02caa7b9b8b9f0
Endryus <endryush@gmail.com>
2024-03-20 19:17:21 -0300

    releasing connection

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -1,15 +1,17 @@
 import { connect } from "./db.js"
 
 async function insertClient (client) {
+  const connection = await connect()
   try {
-    const connection = await connect()
     const sql = 'INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *'
     const values = [client.name, client.cpf, client.phone, client.email, client.address]
     const response = await connection.query(sql, values)
   
     return response?.rows?.[0]
   } catch (error) {
-    next(error);
+    throw error;
+  } finally {
+    connection.release()
   }
 }
 
